3bb674ddeaf5af724cde42f8ad4cfa4af82c29b8
Richard Hui <rhui@hawk.iit.edu>
2018-08-30 21:19:13 -0500

    mini capstone with Restful routing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create({name: "Epyon", price: 12345678, image_url: "https://vignette.wikia.nocookie.net/gundam/images/c/c4/OZ-13MS.Sh.JPG/revision/latest?cb=20120315184429", description: "The OZ-13MS Gundam Epyon is a mobile suit featured in Mobile Suit Gundam Wing. Originally piloted by Heero Yuy, the suit would later be given to Zechs Merquise."})
+Product.create({name: "God Gundam", price: 87654321, image_url: "https://vignette.wikia.nocookie.net/gundam/images/a/a8/God_Gundam_-_Front.jpg/revision/latest?cb=20171014112023", description: "The GF13-017NJII God Gundam (aka G Gundam, Burning Gundam in the English dub) is the titular Mobile Fighter of Mobile Fighter G Gundam. It was the second Mobile Fighter used by Neo Japan in the 13th Gundam Fight, and is piloted by Domon Kasshu."})
