0e109327504dcc41793ec00654e881eccd9c425a
Richard Hui <rhui@hawk.iit.edu>
2018-09-13 21:31:11 -0500

    partner work with Andrew

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,22 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  def calc_subtotal
+    self.subtotal = product.price * quantity
+  end
+
+  def calc_tax
+    self.tax = subtotal * 0.095
+  end
+
+  def calc_total
+    self.total = subtotal + tax
+  end
+
+  def build_totals
+    calc_subtotal
+    calc_tax
+    calc_total
+  end
 end
