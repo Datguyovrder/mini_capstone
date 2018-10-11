e86c0b021868bdbca0b30571d92af2c0c7bcd9ec
Richard Hui <rhui@hawk.iit.edu>
2018-10-11 18:30:45 -0500

    index.js practice

---
+++ public/index.js
@@ -0,0 +1,9 @@
+var propductTemplate = document.querySelector('#product-card');
+var productContainer = document.querySelector('.row')
+
+productContainer.appendChild(propductTemplate.content.cloneNode(true));
+productContainer.appendChild(propductTemplate.content.cloneNode(true));
+productContainer.appendChild(propductTemplate.content.cloneNode(true));
+productContainer.appendChild(propductTemplate.content.cloneNode(true));
+productContainer.appendChild(propductTemplate.content.cloneNode(true));
+productContainer.appendChild(propductTemplate.content.cloneNode(true));
