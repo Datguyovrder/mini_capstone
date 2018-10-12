37ebe3f542102e4cbc4058e6101985e2602b311c
Richard Hui <rhui@hawk.iit.edu>
2018-10-11 19:54:06 -0500

    need to fix

--- public/index.js
+++ public/index.js
@@ -1,9 +1,21 @@
 var propductTemplate = document.querySelector('#product-card');
 var productContainer = document.querySelector('.row')
 
-productContainer.appendChild(propductTemplate.content.cloneNode(true));
-productContainer.appendChild(propductTemplate.content.cloneNode(true));
-productContainer.appendChild(propductTemplate.content.cloneNode(true));
-productContainer.appendChild(propductTemplate.content.cloneNode(true));
-productContainer.appendChild(propductTemplate.content.cloneNode(true));
-productContainer.appendChild(propductTemplate.content.cloneNode(true));
+
+
+axios.get("http://localhost:3000/api/products").then(function(response) {
+  var products = response.data;
+  console.log(products);
+
+  products.forEach(function(product) {
+    var productClone = productTemplate.content.cloneNode(true)
+    
+    productClone.querySelector('.product-name').href = "/products/" + product["id"]
+    productClone.querySelector('.product-name').innerText = product.name;
+    productClone.querySelector('.price').innerText = product.price;
+    // productClone.querySelector('img').src = product.image_url
+    productClone.querySelector('.description').innerText = product.description;
+    
+    productContainer.appendChild(productClone);
+  });
+});
